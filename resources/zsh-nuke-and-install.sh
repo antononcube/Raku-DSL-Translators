763e66d20d6f60f074c7017d916f9f28edd450aa
antonocube <antononcube@gmail.com>
2021-03-28 16:16:25 -0400

    Added missing packages.

--- resources/zsh-nuke-and-install.sh
+++ resources/zsh-nuke-and-install.sh
@@ -5,18 +5,21 @@
 # Comment out if there is no zef:
 #zef nuke site
 
-## Edit the following line to be the correct directory name of the zef repository:
-#cd ~/GitHub/ugexe/zef
-#
-#raku -I. bin/zef install .
-#
-#cd -
+# Edit the following line to be the correct directory name of the zef repository:
+cd ~/GitHub/ugexe/zef
+
+raku -I. bin/zef install .
+
+cd -
 
 ## Install prove6
 #zef install App::Prove6
 
 ## Install Sous-Chef Susana Raku packages
 zef install https://github.com/antononcube/Raku-DSL-Shared.git --force-install --force-test
+zef install https://github.com/antononcube/Raku-DSL-Entity-English-Foods.git --force-install
+zef install https://github.com/antononcube/Raku-DSL-Entity-English-Geographics.git --force-install
+zef install https://github.com/antononcube/Raku-DSL-Entity-English-Jobs.git --force-install
 zef install https://github.com/antononcube/Raku-DSL-English-ClassificationWorkflows.git --force-install --force-test
 zef install https://github.com/antononcube/Raku-DSL-English-DataQueryWorkflows.git --force-install --force-test
 zef install https://github.com/antononcube/Raku-DSL-English-EpidemiologyModelingWorkflows.git --force-install --force-test
@@ -25,4 +28,4 @@ zef install https://github.com/antononcube/Raku-DSL-English-LatentSemanticAnalys
 zef install https://github.com/antononcube/Raku-DSL-English-QuantileRegressionWorkflows.git --force-install --force-test
 zef install https://github.com/antononcube/Raku-DSL-English-RecommenderWorkflows.git --force-install --force-test
 zef install https://github.com/antononcube/Raku-DSL-English-SearchEngineQueries.git --force-install --force-test
-zef install https://github.com/antononcube/Raku-DSL-Shared-Utilities-ComprehensiveTranslation --force-install --force-test
+zef install https://github.com/antononcube/Raku-DSL-Shared-Utilities-ComprehensiveTranslation.git --force-install --force-test
