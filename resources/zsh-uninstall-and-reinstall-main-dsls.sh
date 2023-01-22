e8bdc2b4d181fefdb3e5a6dcc48eeb935928af21
antononcube <antononcube@posteo.net>
2023-01-22 11:07:43 -0500

    test:Script for testing the main DSL modules by un- and re-installation/

---
+++ resources/zsh-uninstall-and-reinstall-main-dsls.sh
@@ -0,0 +1,18 @@
+#! /bin/zsh
+
+## Uninstall
+zef uninstall DSL::English::ClassificationWorkflows
+zef uninstall DSL::English::DataQueryWorkflows
+zef uninstall DSL::English::EpidemiologyModelingWorkflows
+zef uninstall DSL::English::LatentSemanticAnalysisWorkflows
+zef uninstall DSL::English::QuantileRegressionWorkflows
+zef uninstall DSL::English::RecommenderWorkflows
+zef uninstall DSL::Shared
+
+zef install https://github.com/antononcube/Raku-DSL-Shared.git --force-install --/test
+zef install https://github.com/antononcube/Raku-DSL-English-ClassificationWorkflows.git --force-install --/test
+zef install https://github.com/antononcube/Raku-DSL-English-DataQueryWorkflows.git --force-install --/test
+zef install https://github.com/antononcube/Raku-DSL-English-EpidemiologyModelingWorkflows.git --force-install --/test
+zef install https://github.com/antononcube/Raku-DSL-English-LatentSemanticAnalysisWorkflows.git --force-install --/test
+zef install https://github.com/antononcube/Raku-DSL-English-QuantileRegressionWorkflows.git --force-install --/test
+zef install https://github.com/antononcube/Raku-DSL-English-RecommenderWorkflows.git --force-install --/test
